2bc89f54e2a2722c7ddebb2d2adfdc289cb48726
MaximumReality <lorith79@gmail.com>
2026-01-16 12:15:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,37 @@
 const CACHE_NAME = 'sphynx-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'flyazulo.png',
-  'mochkil_front_kart.png',
-  'mochkil_walk_right_cake.png',
-  'logo.png',
-  'hacker-favicon.PNG', 
-  'sw.js'
+  '/sphynx-overdrive/',
+  '/sphynx-overdrive/index.html',
+  '/sphynx-overdrive/manifest.json',
+  '/sphynx-overdrive/flyazulo.png',
+  '/sphynx-overdrive/mochkil_front_kart.png',
+  '/sphynx-overdrive/mochkil_walk_right_cake.png',
+  '/sphynx-overdrive/logo.png',
+  '/sphynx-overdrive/hacker-favicon.PNG',
+  '/sphynx-overdrive/credibility_badge.png'
 ];
 
-// Install service worker and cache assets
+// Install event: cache all assets
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching assets for Maximum Reality...');
-      return cache.addAll(ASSETS);
-    })
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-// Fetch assets from cache, fallback to network
+// Fetch event: serve from cache if available
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
 
-// Activate and clean up old caches
+// Activate event: clean up old caches
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) {
-          console.log('Clearing old cache:', key);
-          return caches.delete(key);
-        }
-      }))
-    )
-  );
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
 });
