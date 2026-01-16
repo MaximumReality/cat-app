11ea247e019bcf6c725d1d532528bcf56698514c
MaximumReality <lorith79@gmail.com>
2026-01-16 05:48:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,25 +6,26 @@ const ASSETS = [
   'mochkil_front_kart.png',
   'mochkil_walk_right_cake.png',
   'logo.png',
-  'hacker-favicon.PNG',
+  'hacker-favicon.PNG', 
   'sw.js'
-];
-
-  
-  // Add any additional files you want cached: e.g., images/icons
 ];
 
 // Install service worker and cache assets
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets for Maximum Reality...');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 // Fetch assets from cache, fallback to network
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
 
@@ -33,7 +34,10 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
+        if (key !== CACHE_NAME) {
+          console.log('Clearing old cache:', key);
+          return caches.delete(key);
+        }
       }))
     )
   );
