26514187501164c8ae523a38fa5fb0178d8780d2
MaximumReality <lorith79@gmail.com>
2026-02-14 00:55:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sphynx-v3'; // Incremented to v3
+const CACHE_NAME = 'sphynx-v4'; // Incremented to v3
 const ASSETS = [
   '/sphynx-overdrive/',
   '/sphynx-overdrive/index.html',
